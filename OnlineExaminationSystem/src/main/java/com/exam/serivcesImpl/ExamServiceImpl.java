0dde09a080ec2dae62c8c54a5f584f53b1ef1f26
Sayantan Manna <96935589+DeveloperSayantan@users.noreply.github.com>
2023-10-20 00:02:59 +0530

    Initial Backend Complete (#2)
    Here is backend connection to connect Database using Spring Boot

---
+++ OnlineExaminationSystem/src/main/java/com/exam/serivcesImpl/ExamServiceImpl.java
@@ -0,0 +1,44 @@
+package com.exam.serivcesImpl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.exam.entity.StudentEntity;
+import com.exam.repository.ExamRepository;
+import com.exam.services.ExamService;
+
+@Service
+public class ExamServiceImpl implements ExamService {
+
+	@Autowired
+	ExamRepository examRepository;
+	
+	@Override
+	public StudentEntity addStudent(StudentEntity studentEntity) {
+		
+		return examRepository.save(studentEntity);
+	}
+
+	@Override
+	public List<StudentEntity> fetchAllStudentEntities() {
+		
+		return examRepository.findAll();
+	}
+
+	@Override
+	public StudentEntity getStudentById(int id) {
+		
+		Optional<StudentEntity> studentOps = examRepository.findById(id);
+		
+		if(studentOps.isPresent()) {
+			
+			return studentOps.get();
+		}
+
+		return null;
+	}
+
+}
