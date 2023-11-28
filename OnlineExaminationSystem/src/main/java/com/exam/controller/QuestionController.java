2f92086a59719966168301962c7e11cd42c5497b
Sayantan Manna <96935589+DeveloperSayantan@users.noreply.github.com>
2023-11-29 00:49:55 +0530

    Add fetch question by Id controller (#7)
    * Add questionList
    
    * Add fetch question by Id controller

--- OnlineExaminationSystem/src/main/java/com/exam/controller/QuestionController.java
+++ OnlineExaminationSystem/src/main/java/com/exam/controller/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exam.entity.QuestionListEntity;
@@ -21,6 +22,12 @@ public class QuestionController {
 	public List<QuestionListEntity> fetchAllQuestions(){
 		
 		return questionService.fetchAllQuestionSet();
+	}
+	
+	@GetMapping("/questionList/{id}")
+	public QuestionListEntity getQuestionById(@PathVariable("id") int id) {
+		
+		return questionService.getQuestionById(id);
 	}
 
 }
